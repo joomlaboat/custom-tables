6fc85d78beb9045f0c7ad61249195a1bc35832ee
JoomlaBoat <ivankomlev@gmail.com>
2020-11-12 18:01:36 -0500

    Header changed

--- admin/models/documentation.php
+++ admin/models/documentation.php
@@ -1,18 +1,12 @@
 <?php
-/*----------------------------------------------------------------------------------|  www.vdm.io  |----/
-				JoomlaBoat.com
-/-------------------------------------------------------------------------------------------------------/
-
-	@version		1.6.1
-	@build			19th July, 2018
-	@created		28th May, 2019
-	@package		Custom Tables
-	@subpackage		view.html.php
-	@author			Ivan Komlev <https://joomlaboat.com>
-	@copyright		Copyright (C) 2018. All Rights Reserved
-	@license		GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html
-
-/------------------------------------------------------------------------------------------------------*/
+/**
+ * CustomTables Joomla! 3.x Native Component
+ * @package Custom Tables
+ * @author Ivan komlev <support@joomlaboat.com>
+ * @link http://www.joomlaboat.com
+ * @copyright Copyright (C) 2018-2020. All Rights Reserved
+ * @license GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html
+ **/
 
 // no direct access
 defined('_JEXEC') or die('Restricted access');
@@ -22,30 +16,4 @@ jimport('joomla.application.component.modeladmin');
 
 class CustomTablesModelDocumentation extends JModelList
 {
-	/*
-	protected $text_prefix = 'COM_CUSTOMTABLES';
-	public $typeAlias = 'com_customtables.documentation';
-
-	public $es;
-	public $imagefolder;//="images/esoptimages";
-
-*/
-	
-	//public function getForm($data = array(), $loadData = false)
-	//{
-		/*
-		// Get the form.
-		$form = $this->loadForm('com_customtables.documentation', 'documentation', array('control' => 'jform', 'load_data' => false));
-
-		if (empty($form))
-		{
-			return false;
-		}
-
-
-		return $form;
-		*/
-	//}
-	
-
 }
