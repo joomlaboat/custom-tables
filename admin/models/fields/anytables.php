99d66a92a3818adbddc7750cc7409c42b5c0a219
JoomlaBoat <ivankomlev@gmail.com>
2022-09-15 08:03:17 -0500

    Documentation: {{ html.limit }} tag description improved.

--- admin/models/fields/anytables.php
+++ admin/models/fields/anytables.php
@@ -44,30 +44,30 @@ class JFormFieldAnyTables extends JFormFieldList
     protected function getListOfExistingTables()
     {
         $db = Factory::getDBO();
+        $prefix = $db->getPrefix();
 
         if ($db->serverType == 'postgresql') {
             $wheres = array();
             $wheres[] = 'table_type = \'BASE TABLE\'';
             $wheres[] = 'table_schema NOT IN (\'pg_catalog\', \'information_schema\')';
-            $wheres[] = 'POSITION(\'' . $db->getPrefix() . 'customtables_\' IN table_name)!=1';
-            $wheres[] = 'table_name!=\'' . $db->getPrefix() . 'user_keys\'';
-            $wheres[] = 'table_name!=\'' . $db->getPrefix() . 'user_usergroup_map\'';
-            $wheres[] = 'table_name!=\'' . $db->getPrefix() . 'usergroups\'';
-            $wheres[] = 'table_name!=\'' . $db->getPrefix() . 'users\'';
+            $wheres[] = 'POSITION(\'' . $prefix . 'customtables_\' IN table_name)!=1';
+            $wheres[] = 'table_name!=\'' . $prefix . 'user_keys\'';
+            $wheres[] = 'table_name!=\'' . $prefix . 'user_usergroup_map\'';
+            $wheres[] = 'table_name!=\'' . $prefix . 'usergroups\'';
+            $wheres[] = 'table_name!=\'' . $prefix . 'users\'';
 
             $query = 'SELECT table_name FROM information_schema.tables WHERE ' . implode(' AND ', $wheres) . ' ORDER BY table_name';
         } else {
             $conf = Factory::getConfig();
             $database = $conf->get('db');
 
-
             $wheres = array();
             $wheres[] = 'table_schema=\'' . $database . '\'';
-            $wheres[] = '!INSTR(TABLE_NAME,\'' . $db->getPrefix() . 'customtables_\')';
-            $wheres[] = 'TABLE_NAME!=\'' . $db->getPrefix() . 'user_keys\'';
-            $wheres[] = 'TABLE_NAME!=\'' . $db->getPrefix() . 'user_usergroup_map\'';
-            $wheres[] = 'TABLE_NAME!=\'' . $db->getPrefix() . 'usergroups\'';
-            $wheres[] = 'TABLE_NAME!=\'' . $db->getPrefix() . 'users\'';
+            $wheres[] = '!INSTR(TABLE_NAME,\'' . $prefix . 'customtables_\')';
+            $wheres[] = 'TABLE_NAME!=\'' . $prefix . 'user_keys\'';
+            $wheres[] = 'TABLE_NAME!=\'' . $prefix . 'user_usergroup_map\'';
+            $wheres[] = 'TABLE_NAME!=\'' . $prefix . 'usergroups\'';
+            $wheres[] = 'TABLE_NAME!=\'' . $prefix . 'users\'';
 
             $query = 'SELECT TABLE_NAME AS table_name FROM information_schema.tables WHERE ' . implode(' AND ', $wheres) . ' ORDER BY TABLE_NAME';
         }
