fab040be7a3bbffc1e80e08526ae3b4714d407e4
joomlaboat <ivankomlev@gmail.com>
2024-06-03 16:36:02 -0800

    Blob Filed type - Saving Blob file fixed.

--- site/libraries/customtables/records/blob.php
+++ site/libraries/customtables/records/blob.php
@@ -21,18 +21,18 @@ class Save_blob
     public Field $field;
     var ?array $row_new;
 
-    function __construct(CT &$ct, Field $field, ?array $row_new)
+    function __construct(CT &$ct, Field $field, ?array &$row_new)
     {
         $this->ct = &$ct;
         $this->field = $field;
-        $this->row_new = $row_new;
+        $this->row_new = &$row_new;//It's important to pass a reference because file name maybe saved to another field
     }
 
     /**
      * @throws Exception
      * @since 3.3.3
      */
-    function saveFieldSet(?string $listing_id): ?array
+    function saveFieldSet(): ?array
     {
         $newValue = null;
 
@@ -57,25 +57,23 @@ class Save_blob
                 $this->row_new[$fileNameField] = null;
 
         } elseif ($value !== null) {
-            $newValue = ['value' => strlen($value)];
+            $newValue = ['value' => $value];
 
             if ($fileNameField != '') {
-                if (!isset($this->row_new[$fileNameField])) {
-                    $file_id = common::inputPostString($this->field->comesfieldname, '', 'create-edit-record');
+                $file_id = common::inputPostString($this->field->comesfieldname, '', 'create-edit-record');
 
-                    //Delete temporary file name parts
-                    //Example: ct_1702267688_PseAH3r3Cy91VhQbh6hzw7bchYW5rK51sD_001_Li-R6ong7bo_LOI_PE1214762_26032019_c1b1121b122.doc
-                    //Cleaned: 001_Li-Ron7gbo_LOI_PE1214762_26032019_c1b1121b122.doc
-                    $file_name_parts = explode('_', $file_id);
+                //Delete temporary file name parts
+                //Example: ct_1702267688_PseAH3r3Cy91VhQbh6hzw7bchYW5rK51sD_001_Li-R6ong7bo_LOI_PE1214762_26032019_c1b1121b122.doc
+                //Cleaned: 001_Li-Ron7gbo_LOI_PE1214762_26032019_c1b1121b122.doc
+                $file_name_parts = explode('_', $file_id);
+                if (count($file_name_parts) > 3 and $file_name_parts[0] == 'ct')
                     $file_name = implode('_', array_slice($file_name_parts, 3));
+                else
+                    $file_name = $file_id;
 
-                    $this->row_new[$fileNameField] = $file_name;
-                }
+                $this->row_new[$fileNameField] = $file_name;
             }
-
-            $this->row_new[$this->field->realfieldname] = $value;
         }
-
         return $newValue;
     }
 }
