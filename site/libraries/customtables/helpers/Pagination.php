d53db2a258223971b19ff6bbe84be313208ab62d
joomlaboat <ivankomlev@gmail.com>
2025-03-04 19:20:03 -0900

    {{ html.pagination }} enabled for WP.

--- site/libraries/customtables/helpers/Pagination.php
+++ site/libraries/customtables/helpers/Pagination.php
@@ -54,6 +54,131 @@ class Pagination
 		$url = common::curPageURL();
 		$url = CTMiscHelper::deleteURLQueryOption($url, $this->prefix . 'start');
 
+		$pages = $this->getPages();
+		if ($pages['total_pages'] <= 1) return '';
+
+		$paginationClass = defined('WPINC') ? 'wp-pagenavi' : 'pagination';
+		$pageItemClass = defined('WPINC') ? 'pager' : 'page-item';
+		$pageLinkClass = defined('WPINC') ? 'page-numbers' : 'page-link';
+
+		$html = '<ul class="' . $paginationClass . '">';
+
+		$html .= '<li class="' . ($pages['current_page'] == 1 ? 'disabled ' : '') . $pageItemClass . '"><a class="' . $pageLinkClass . '" href="' . ($pages['current_page'] > 1 ? $this->buildUrl($url, 1) : '#') . '">' . ($this->showArrowIcons ? Icons::iconStart($this->toolbarIcons) : 'Start') . '</a></li>';
+
+		$html .= '<li class="' . ($pages['prev_page'] ? $pageItemClass : 'disabled ' . $pageItemClass) . '"><a class="' . $pageLinkClass . '" href="' . ($pages['prev_page'] ? $this->buildUrl($url, $pages['prev_page']) : '#') . '">' . ($this->showArrowIcons ? Icons::iconPrev($this->toolbarIcons) : 'Prev') . '</a></li>';
+
+		foreach ($pages['pages'] as $page) {
+			if ($page === '...') {
+				$html .= "<li class='disabled $pageItemClass'><span class='$pageLinkClass'>...</span></li>";
+			} else {
+				$class = $page == $pages['current_page'] ? 'active ' . $pageItemClass : $pageItemClass;
+				$ariaCurrent = $page == $pages['current_page'] ? ' aria-current="true"' : '';
+				$html .= "<li class='$class'><a title='$page' href=\"" . $this->buildUrl($url, $page) . "\" class='$pageLinkClass' $ariaCurrent>$page</a></li>";
+			}
+		}
+
+		$html .= '<li class="' . ($pages['next_page'] ? $pageItemClass : 'disabled ' . $pageItemClass) . '"><a class="' . $pageLinkClass . '" href="' . ($pages['next_page'] ? $this->buildUrl($url, $pages['next_page']) : '#') . '">' . ($this->showArrowIcons ? Icons::iconNext($this->toolbarIcons) : 'Next') . '</a></li>';
+
+		$html .= '<li class="' . ($pages['current_page'] == $pages['total_pages'] ? 'disabled ' : '') . $pageItemClass . '"><a class="' . $pageLinkClass . '" href="' . ($pages['current_page'] < $pages['total_pages'] ? $this->buildUrl($url, $pages['total_pages']) : '#') . '">' . ($this->showArrowIcons ? Icons::iconEnd($this->toolbarIcons) : 'End') . '</a></li>';
+
+		$html .= '</ul>';
+		return $html;
+	}
+
+	public function getPages(): array
+	{
+		$pagesTotal = (int)ceil($this->total / $this->limit);
+		$currentPage = (int)($this->limitStart / $this->limit) + 1;
+		$pages = [];
+
+		if ($pagesTotal <= 7) {
+			$pages = range(1, $pagesTotal);
+		} else {
+			$pages = [1, 2];
+			if ($currentPage > 4) {
+				$pages[] = '...';
+			}
+
+			for ($i = max(3, $currentPage - 1); $i <= min($pagesTotal - 2, $currentPage + 1); $i++) {
+				$pages[] = $i;
+			}
+
+			if ($currentPage < $pagesTotal - 3) {
+				$pages[] = '...';
+			}
+
+			$pages[] = $pagesTotal - 1;
+			$pages[] = $pagesTotal;
+		}
+
+		return [
+			'total_pages' => $pagesTotal,
+			'current_page' => $currentPage,
+			'prev_page' => $currentPage > 1 ? $currentPage - 1 : null,
+			'next_page' => $currentPage < $pagesTotal ? $currentPage + 1 : null,
+			'pages' => $pages
+		];
+	}
+
+	private function buildUrl(string $url, int $page): string
+	{
+		$start = ($page - 1) * $this->limit;
+		return $url . (strpos($url, '?') === false ? '?' : '&') . $this->prefix . 'start=' . $start;
+	}
+
+	protected function renderWP(): string
+	{
+		$url = common::curPageURL();
+		$url = CTMiscHelper::deleteURLQueryOption($url, $this->prefix . 'start');
+
+		$pages = $this->getPages();
+		if ($pages['total_pages'] <= 1) return '';
+
+		$paginationClass = 'navigation pagination';
+		$pageContainerClass = 'nav-links';
+		$pageItemClass = 'page-numbers';
+
+		$html = "<div class='$paginationClass'><div class='$pageContainerClass'>";
+
+		if ($pages['current_page'] > 1) {
+			$html .= '<a class="prev page-numbers" href="' . $this->buildUrl($url, 1) . '">' . ($this->showArrowIcons ? Icons::iconStart($this->toolbarIcons) : '« First') . '</a>';
+		}
+
+		if ($pages['prev_page']) {
+			$html .= '<a class="prev page-numbers" href="' . $this->buildUrl($url, $pages['prev_page']) . '">' . ($this->showArrowIcons ? Icons::iconPrev($this->toolbarIcons) : '‹ Prev') . '</a>';
+		}
+
+		foreach ($pages['pages'] as $page) {
+			if ($page == '...') {
+				$html .= '<span class="page-numbers dots">…</span>';
+			} else {
+				$class = $page == $pages['current_page'] ? 'current' : '';
+				$ariaCurrent = $page == $pages['current_page'] ? ' aria-current="page"' : '';
+
+				if ($page == $pages['current_page'])
+					$html .= "<span class='$pageItemClass $class' $ariaCurrent>$page</span>";
+				else
+					$html .= "<a href=\"" . $this->buildUrl($url, $page) . "\" class='$pageItemClass $class' $ariaCurrent>$page</a>";
+			}
+		}
+
+		if ($pages['next_page']) {
+			$html .= '<a class="next page-numbers" href="' . $this->buildUrl($url, $pages['next_page']) . '">' . ($this->showArrowIcons ? Icons::iconNext($this->toolbarIcons) : 'Next ›') . '</a>';
+		}
+
+		if ($pages['current_page'] < $pages['total_pages']) {
+			$html .= '<a class="next page-numbers" href="' . $this->buildUrl($url, $pages['total_pages']) . '">' . ($this->showArrowIcons ? Icons::iconEnd($this->toolbarIcons) : 'Last »') . '</a>';
+		}
+
+		$html .= "</div></div>";
+		return $html;
+	}
+
+	public function renderJoomla_Old(): string
+	{
+		$url = common::curPageURL();
+		$url = CTMiscHelper::deleteURLQueryOption($url, $this->prefix . 'start');
+
 
 		$pages = $this->getPages();
 		if ($pages['total_pages'] <= 1) return '';
@@ -94,7 +219,7 @@ class Pagination
 		return $html;
 	}
 
-	public function getPages(): array
+	public function getPagesOld(): array
 	{
 		$pagesTotal = (int)ceil($this->total / $this->limit);
 		$currentPage = (int)($this->limitStart / $this->limit) + 1;
@@ -108,13 +233,7 @@ class Pagination
 		];
 	}
 
-	private function buildUrl(string $url, int $page): string
-	{
-		$start = ($page - 1) * $this->limit;
-		return $url . (strpos($url, '?') === false ? '?' : '&') . $this->prefix . 'start=' . $start;
-	}
-
-	protected function renderWP(): string
+	protected function renderWP_Old(): string
 	{
 		$url = common::curPageURL();
 		$url = CTMiscHelper::deleteURLQueryOption($url, $this->prefix . 'start');
